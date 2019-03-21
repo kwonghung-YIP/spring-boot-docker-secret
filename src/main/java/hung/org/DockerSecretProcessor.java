c06a3ef9de7be67c249f5bf10d9dd45c9859e733
Kwong Hung Yip <kwonghung.yip@gmail.com>
2019-03-22 01:39:46 +0800

    Update EnvironmentPostProcessor implementation

--- src/main/java/hung/org/DockerSecretProcessor.java
+++ src/main/java/hung/org/DockerSecretProcessor.java
@@ -1,14 +1,19 @@
 package hung.org;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.context.properties.source.MapConfigurationPropertySource;
 import org.springframework.boot.env.EnvironmentPostProcessor;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
 import org.springframework.stereotype.Component;
+import org.springframework.util.FileCopyUtils;
 
 @Component
 public class DockerSecretProcessor implements EnvironmentPostProcessor {
@@ -16,14 +21,38 @@ public class DockerSecretProcessor implements EnvironmentPostProcessor {
 	@Override
 	public void postProcessEnvironment(ConfigurableEnvironment environment, SpringApplication application) {
 		
-		
 		String bindPathPpty = environment.getProperty("docker-secret.bind-path");
+		
 		if (bindPathPpty!=null) {
 			Path bindPath = Paths.get(bindPathPpty);
-			if (Files.isDirectory(bindPath)) {				
-				bindPath.forEach(file -> {
-					
-				});
+			if (Files.isDirectory(bindPath)) {
+				Map<String,Object> dockerSecrets;
+				try {
+					dockerSecrets = 
+					  Files.list(bindPath)
+					    .collect(
+					      Collectors.toMap(
+					        path -> {
+					        	File secretFile = path.toFile();
+					        	return "docker-secret-" + secretFile.getName();
+					        },
+					        path -> {
+					        	File secretFile = path.toFile();
+								try {
+									byte[] content = FileCopyUtils.copyToByteArray(secretFile);
+									return new String(content);
+								} catch (IOException e) {
+									throw new RuntimeException(e);
+								}
+					        }
+					      ));
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+				MapPropertySource pptySource = new MapPropertySource("docker-secrets",dockerSecrets);
+				
+				environment.getPropertySources().addLast(pptySource);
+				
 			}
 		}			
 	}
